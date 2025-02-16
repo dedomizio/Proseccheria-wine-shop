cd945c61fd14a00668e5cac91ff0a7ffd62c0070
dedomizio <gulisanomattia@yahoo.it>
2025-02-16 10:01:11 +0100

    Create cartContext.js

---
+++ components/cartContext.js
@@ -0,0 +1,25 @@
+import { createContext, useState, useContext } from "react";
+
+const CartContext = createContext();
+
+export function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (wine) => {
+    setCart((prevCart) => [...prevCart, wine]);
+  };
+
+  const removeFromCart = (index) => {
+    setCart((prevCart) => prevCart.filter((_, i) => i !== index));
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart() {
+  return useContext(CartContext);
+}

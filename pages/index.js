d04b21fcf9f272c382735fb0f20373063cc2acf5
dedomizio <gulisanomattia@yahoo.it>
2025-02-16 01:09:07 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-import WineShop from "../components/WineShop";
+import Link from "next/link";
 
 export default function Home() {
-  return <WineShop />;
+  return (
+    <div className="p-6">
+      <h1 className="text-4xl font-bold">Benvenuto alla Proseccheria</h1>
+      <p className="mt-4 text-lg">Scopri i nostri migliori vini e acquista direttamente online!</p>
+      <Link href="/shop">
+        <button className="mt-6 bg-blue-500 text-white p-3 rounded-lg">Vai al negozio</button>
+      </Link>
+    </div>
+  );
 }
